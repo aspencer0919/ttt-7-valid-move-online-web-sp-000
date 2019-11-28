cfbd5e29e3723befc695cc53c00694d0b3b807d0
Adam Spencer <aspencer0919@gmail.com>
2019-11-28 19:49:08 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,15 +3,15 @@ index = 1
 
 def valid_move?(board, index)
   def position_taken?(array, ind)
-    if array[ind] == " " || array[ind] == "" || array[ind] == nil
+    if board[index] == " " || board[index] == "" || board[index] == nil
       return false
     else
       return true
     end
   end
 
-  def on_board?(num)
-    if num.between?(0, 8) == true
+  def on_board?(index)
+    if index.between?(0, 8) == true
       return true
     else
       return false
